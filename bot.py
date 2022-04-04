967f45fdad0d36263b733de08a171c00c548f1ea
Alexey Bulgakov <allbull33@gmail.com>
2022-04-04 13:32:46 +0300

    Add changes

--- bot.py
+++ bot.py
@@ -3,25 +3,26 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import TOKEN
-
+import db_reader
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
-
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
+#@dp.message_handler()
+#async def echo_message(msg: types.Message):
+ #   await bot.send_message(msg.from_user.id, msg.text)
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+@dp.message_handler(commands=['photo'])
+async def process_photo_command(message: types.Message):
+    await bot.send_photo(message.from_user.id, db_reader.read_blob_data(2))
 
 if __name__ == '__main__':
     executor.start_polling(dp)
---
+++ db_reader.py
@@ -0,0 +1,28 @@
+import sqlite3, os
+
+def write_to_file(data, filename):
+    with open(filename, 'wb') as file:
+        file.write(data)
+    print("Данные сохранены")
+
+def read_blob_data(emp_id):
+    try:
+        sqlite_connection = sqlite3.connect('testDB.db')
+        cursor = sqlite_connection.cursor()
+        print("Подключен к БД")
+
+        sql_fetch_blob_query = """SELECT * from test where id = ?"""
+        cursor.execute(sql_fetch_blob_query, (emp_id,))
+        record = cursor.fetchall()
+        for row in record:
+            print("Id = ", row[0])
+            photo = row[1]
+            return photo
+
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
